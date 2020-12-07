00fa0125622a86bd6c0bee90adcf834a596cc16c
Ricardo Paz <ricardopsf@apple.com>
2020-12-07 01:12:46 -0300

    feat: remove unused plugin.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
 const withImages = require('next-images')
-const withPlugins = require('next-compose-plugins')
 
-module.exports = withPlugins([
-  [{
-    webpackDevMiddleware(config) {
-      config.watchOptions = {
-        ignored: [
-          /[\\/]\.next[\\/]/,
-          /[\\/]node_modules[\\/]/
-        ]
-      }
-
-      return config
-    }
-  }],
-  [withImages({ esModule: true })]
-])
+module.exports = withImages({ esModule: true })
