947715abdcc021f48e06f0ab7f82e84209baf8c6
Ricardo Paz <ricardopsf@apple.com>
2020-12-09 22:59:03 -0300

    feat: insert SEO on index page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,48 @@ import React from 'react'
 import App from 'next/app'
 import Head from 'next/head'
 import ThemeContainer from '../config/theme/ThemeContainer'
+import { NextSeo, LogoJsonLd, SocialProfileJsonLd } from 'next-seo'
 
 export default class MainApp extends App {
   render(): JSX.Element {
     const { Component, pageProps } = this.props
+    const description = `Economize até 80% em sua contabilidade de forma prática
+    e acessível, tudo isso de forma digital. Mantenha o foco e seus
+    recursos voltados a sua operação.`
 
     return (
       <ThemeContainer>
+        <NextSeo
+          description={description}
+          canonical="https://www.symplecont.com/"
+          openGraph={{
+            url: 'https://www.symplecont.com/',
+            title: 'SympleCont - Contabilidade Digital',
+            description: description,
+            images: [
+              {
+                url: 'https://imghub.io/i/N231d',
+                width: 500,
+                height: 500,
+                alt: 'SympleCont Logo'
+              }
+            ],
+            site_name: 'SympelCont'
+          }}
+        />
+        <LogoJsonLd
+          logo="https://imghub.io/i/N231d"
+          url="http://www.symplecont.com"
+        />
+        <SocialProfileJsonLd
+          type="Person"
+          name="SympleCont"
+          url="http://www.symplecont.com"
+          sameAs={[
+            'http://www.facebook.com/symplecont',
+            'http://instagram.com/symplecont'
+          ]}
+        />
         <Head>
           <meta
             name="viewport"
